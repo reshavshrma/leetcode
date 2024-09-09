70d3a0e53c7f067f5ae26729169fc99e94a3ec47
Unaccessed <2022bit061@sggs.ac.in>
2024-09-09 18:35:15 +0530

    10

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,17 @@
+'''
+Given a string s, find the length of the longest substring without repeating characters.
+'''
+
+def lengthOfLongestSubstring(s):
+	l, r = 0, 0
+	max_length = 0
+	
+	while r < len(s):
+		while s[r] in s[l:r]:
+			l += 1
+		max_length = max(r-l+1, max_length)
+		r += 1
+	return max_length 
+
+print(lengthOfLongestSubstring('abcabcbb'))
+print(lengthOfLongestSubstring('bbbbb'))
