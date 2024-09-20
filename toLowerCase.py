17b1b9f8f42dbb9702b891293941ba7a0b7cba6a
Unaccessed <2022bit061@sggs.ac.in>
2024-09-20 23:02:49 +0530

    .

---
+++ toLowerCase.py
@@ -0,0 +1,9 @@
+'''
+Given a string s, return the string after replacing 
+every uppercase letter with the same lowercase letter.
+'''
+
+def toLowerCase(s):
+	return s.lower()
+	
+print(toLowerCase("Hello"))
