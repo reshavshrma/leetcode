141b5a738c3fc48eaf26a365a495597e723b20cd
Unaccessed <2022bit061@sggs.ac.in>
2024-09-25 22:50:15 +0530

    .

---
+++ addStrings.py
@@ -0,0 +1,12 @@
+'''
+Given two non-negative integers, num1 and num2 represented as string, 
+return the sum of num1 and num2 as a string. You must solve the problem 
+without using any built-in library for handling large integers 
+(such as BigInteger). You must also not convert the inputs to integers directly.
+'''
+
+def addStrings(num1, num2):
+	sum = int(num1) + int(num2)
+	return str(sum)
+	
+print(addStrings('11', '123'))
