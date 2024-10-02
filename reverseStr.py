0b19dacdcda2396a894b0960ecac63b5711e6914
Unaccessed <2022bit061@sggs.ac.in>
2024-10-03 04:16:04 +0530

    .

---
+++ reverseStr.py
@@ -0,0 +1,17 @@
+'''
+Given a string s and an integer k, reverse the first k characters 
+for every 2k characters counting from the start of the string.
+If there are fewer than k characters left, reverse all of them. 
+If there are less than 2k but greater than or equal to k characters, 
+then reverse the first k characters and leave the other as original.
+'''
+
+def reverseStr(s, k):
+	arr = list(s)
+	
+	for i in range(0, len(arr), 2*k):
+		arr[i:i+k] = reversed(arr[i:i+k])
+	return ''.join(arr)
+	
+s, k = "abcdefg", 2
+print(reverseStr(s, k))
