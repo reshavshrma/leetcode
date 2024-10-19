94c8fce23107b8d31feb6f55048d897d7065e5d2
Unaccessed <2022bit061@sggs.ac.in>
2024-10-20 00:12:05 +0530

    upload

---
+++ move_zeroes.py
@@ -0,0 +1,10 @@
+def moveZeroes(self, nums):
+        non_zero_index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0
