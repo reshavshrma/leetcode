9cdc251097d42648351ffcb5a61859a04b098afb
Unaccessed <2022bit061@sggs.ac.in>
2024-11-01 21:47:50 +0530

    upload

---
+++ pascalTriangle.py
@@ -0,0 +1,21 @@
+'''
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+'''
+
+def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+        prevRows.append(newRow)
+        return prevRows
+        
+        
