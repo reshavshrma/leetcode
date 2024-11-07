ddffc34f95ff35100132fbe33548d4214c488dbb
Unaccessed <2022bit061@sggs.ac.in>
2024-11-08 03:44:44 +0530

    upload

---
+++ permu.py
@@ -0,0 +1,13 @@
+def permute(self, nums):
+        def backtrack(start):
+            if start == len(nums):
+                permutations.append(nums[:])
+            else:
+                for i in range(start, len(nums)):
+                    nums[start], nums[i] = nums[i], nums[start]
+                    backtrack(start + 1)
+                    nums[start], nums[i] = nums[i], nums[start]
+
+        permutations = []
+        backtrack(0)
+        return permutations
